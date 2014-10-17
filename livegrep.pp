a92cab16f06223335466a7c12d1981da845dcbea
Spencer Krum <nibz@cat.pdx.edu>
2014-10-16 18:53:37 -0700

    working

--- livegrep.pp
+++ livegrep.pp
@@ -35,22 +35,24 @@
     provider    => 'shell',
     #refreshonly => true,
     cwd         => '/disk/blob/home/nibz/sandbox/livegrep/livegrep_web',
-    command     => '/usr/bin/docker build --tag=livegrep:livegrep_web .',
+    command     => '/usr/bin/docker build --tag=livegrep:web .',
   } ~>
 
-  docker::run { 'livegrep:codeesearch_worker':
+  docker::run { 'codesearch_worker':
     image           => 'livegrep:codesearch_worker',
-    command         => '/bin/sh -c "while true; do echo hello world; sleep 1; done"', 
-    expose          => ['9999'],
+    use_name        => true,
+    volumes         => ['/disk/blob/home/nibz/sandbox/livegrep/git_repos:/git_repos', '/disk/blob/home/nibz/sandbox/livegrep/indexes:/indexes'],
     dns             => ['8.8.8.8', '8.8.4.4'],
     restart_service => true,
   } ~>
 
-  docker::run { 'livegrep:livegrep_web':
-    image           => 'livegrep:livegrep_web',
-    command         => '/bin/sh -c "while true; do echo hello world; sleep 1; done"', 
-    expose          => ['9999'],
+  docker::run { 'web':
+    image           => 'livegrep:web',
+    use_name        => true,
+    links           => ['codesearch_worker:codesearch_worker'],
+    volumes         => ['/disk/blob/home/nibz/sandbox/livegrep/git_repos:/git_repos', '/disk/blob/home/nibz/sandbox/livegrep/indexes:/indexes'],
     dns             => ['8.8.8.8', '8.8.4.4'],
+    ports           => ['131.252.216.15:8910:8910'],
     restart_service => true,
   }
 
